a1edd8a4f1e82835f8b8899096c4220d2535e08d
Kuei-Jung Hu <akueisara39@gmail.com>
2020-12-30 22:49:39 +0900

    Initialize iOS project HuliPizza

---
+++ HuliPizza/HuliPizza/HuliPizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuliPizzaApp.swift
+//  HuliPizza
+//
+//  Created by Kuei-Jung Hu on 2020/12/30.
+//
+
+import SwiftUI
+
+@main
+struct HuliPizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+			ContentView()
+        }
+    }
+}
